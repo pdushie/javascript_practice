3c831c1088bdaac84694e047d537965105dce8da
Philip Dushie <philip.dushie@gmail.com>
2024-11-02 23:07:39 -0300

    Created async function to display random cards

---
+++ thirdPartyApi/README.md
@@ -0,0 +1,3 @@
+# Deck Of Cards API Practice
+I will practice how to interact with APIs using JavaScript's fetch method with an asynchronous function.
+On successful fetch the image of the card drawn from the deck of card will be dislpayed.
---
+++ thirdPartyApi/deckOfCards.js
@@ -0,0 +1,32 @@
+// Will practice working with third party APIs here
+// Asynchronous functions will be used
+// Asynchronous functions allow the program to continue running and perform other tasks while they fetch required data.
+// when the asynchronous function completes its task, it calls back the program
+async function getData() {
+  try {
+    const response = await fetch('https://www.deckofcardsapi.com/api/deck/new/draw/?count=1');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    if (error) {
+      console.error(error.message);
+    }
+  }
+}
+
+// Create another asynchronous function which waits for getData to complete its data fetch
+
+async function displayData() {
+  data = await getData();
+  //console.log(data);
+  return data;
+}
+
+let displayCard = document.querySelector('#showCard');
+
+async function getCardImage () {
+  data = await displayData();
+  displayCard.innerHTML = `<img src=${JSON.stringify(data.cards[0].image)} alt="Card Image">`;
+}
+
+document.getElementById("showImage").addEventListener("click", ()=>{getCardImage()});
