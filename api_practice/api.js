850fb70c3dc88aa9e15abf70ce628743478009e7
Philip Dushie <philip.dushie@gmail.com>
2024-11-22 10:19:41 -0400

    created endpoint to get all employees

--- api_practice/api.js
+++ api_practice/api.js
@@ -3,7 +3,42 @@ import Database from 'better-sqlite3';
 import Joi from 'joi';
 
 // Create instance of express function
-app = new express();
+const app = new express();
+
+// Declare port number on which express would listen for incomming connections
+const port = 8080;
+
+// Connect to database using better-sqlite3
+const db = Database('./database/chinook.sqlite', { fileMustExist: true });
 
 // Serve static files using express
 app.use(express.static("public"));
+
+// Create endpoint to get all employees from DB
+app.get('/api/employees', (req, res) => {
+    try {
+        const statement = db.prepare("SELECT * FROM employees")
+        const result = statement.all();
+
+        // Send result of select
+        res.send(result);
+    } catch (err) {
+        //Log error to console
+        console.log(err);
+
+        // Send error response to user
+        res.status(500).send({
+            "Message": "Try again later",
+            "error": err.code
+        });
+    }
+
+
+});
+
+
+
+// Configure express to listen on port 8080
+app.listen(port, () => {
+    console.log("Server listening on port", port);
+});
